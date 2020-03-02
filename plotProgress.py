2f707eab1ac1a523617c7ea2c9e1f4d7fb46b794
Tyler Burch <burcht11@gmail.com>
2020-03-02 09:35:14 -0600

    Add time-delta plots

--- plotProgress.py
+++ plotProgress.py
@@ -4,6 +4,8 @@ import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 import os
 import sys
+import numpy as np
+import seaborn as sns
 savedir = os.getcwd()+"/"
 
 df = pd.read_csv(os.getcwd()+"/"+sys.argv[1])
@@ -61,3 +63,46 @@ plt.savefig(savedir+"progress.png")
 fig = plt.gcf()
 fig.set_size_inches(2*fig.get_figheight(),fig.get_figheight(),forward=True)
 plt.savefig(savedir+"progress_twitter.png")
+plt.close()
+
+##### Time Delta Plot ####
+def make_pages_plot(axis):
+    df.plot(y='pagecount',legend=None, color=pc_color, ax=axis,label="Raw Data")
+    done_resample_df =  df.resample('w').max()
+    done_resample_df['pagecount'].plot(figsize=(12,8), color='salmon',label="Rolling 7-day Max", ax=axis)
+    slope = pd.Series(np.gradient(done_resample_df["pagecount"].values), done_resample_df.index, name='slope')
+    plt.sca(axis)
+    plt.plot(done_resample_df['pagecount'].diff(), color="forestgreen",label="Rolling 7-Day Change")
+    plt.ylabel("Pages",fontsize=14)
+
+def make_words_plot(axis):
+    df.plot(y='wordcount',legend=None, color=pc_color, ax=axis,label="Raw Data")
+    done_resample_df =  df.resample('w').max()
+    done_resample_df['wordcount'].plot(figsize=(12,8), color='salmon',label="Rolling 7-day Max",ax=axis)
+    slope = pd.Series(np.gradient(done_resample_df["wordcount"].values), done_resample_df.index, name='slope')
+    plt.sca(axis)
+    plt.plot(done_resample_df['wordcount'].diff(), color="forestgreen",label="Rolling 7-Day Change")
+    plt.ylabel("Words",fontsize=14)
+
+def make_format():
+    sns.despine()
+    plt.ylim(bottom=0)
+    plt.xticks(fontsize=12)
+    plt.yticks(fontsize=12)
+
+fig, (ax1,ax2) = plt.subplots(ncols=1, nrows=2, sharex=True)
+make_pages_plot(ax1)
+make_format()
+make_words_plot(ax2)
+make_format()
+plt.xlabel("Date",fontsize=14)
+plt.legend(bbox_to_anchor=(1.2,1), loc="center",frameon=False,fontsize=14)
+
+days = mdates.DayLocator()
+months = mdates.MonthLocator()  # every month
+years_fmt = mdates.DateFormatter('%b %Y')
+ax2.xaxis.set_major_locator(months)
+ax2.xaxis.set_major_formatter(years_fmt)
+plt.minorticks_off()
+plt.tight_layout()
+plt.savefig(savedir+'delta_plots.png')
